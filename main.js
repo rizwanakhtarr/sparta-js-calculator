74ccff2d0dd2b50227759812ac61274494d5b86c
unknown <rakhtar@spartaglobal.com>
2018-07-01 15:44:38 +0100

    advanced calc

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var settingOne = "1";
 var settingTwo = "2";
 
 
-if (settingTwo == "2") {
+if (setting == "2") {
   var option = prompt("Would you like to (m)ultiply, (d)ivide, (a)dd or (s)ubstract?");
 
   var num1 = prompt('number1?');
@@ -28,5 +28,22 @@ if (settingTwo == "2") {
   } else if (option == "a") {
     alert(num1convert + num2convert);
   }
+
+} else if (setting == "1") {
+  var option2 = prompt("Would you like to (sq)root, (p)ower?");
+
+  var num1 = prompt('number1?');
+  var num1convert = parseFloat(num1);
+  var num2 = prompt('to the power of which number?');
+  var num2convert = parseFloat(num2);
+
+  if (option2 == "p") {
+    var power = Math.pow(num1, num2);
+    alert(power);
+  } else if (option2 == "sq") {
+    var root = Math.sqrt(num1, num2);
+    alert(root);
+  }
+
 
 }
